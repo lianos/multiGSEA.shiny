234fba6651776eeb2657af1cd3b076254d7fd461
Steve Lianoglou <lianoglou.steve@gene.com>
2017-11-21 22:24:28 -0800

    removing bitrot

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -14,4 +14,4 @@ library(dtplyr)
 ## By default shiny limits upload size to 5 MB, let's change this to 30MB
 ## (which is kind of big, no?)
 options(shiny.maxRequestSize=30*1024^2)
-options(multiGSEA.df.return='data.table')
+
